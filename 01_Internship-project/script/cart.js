8c8f75135f39b28db466a6596e24f6f866f538eb
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-02-28 20:34:24 +0530

    new commit again

--- 01_Internship-project/script/cart.js
+++ 01_Internship-project/script/cart.js
@@ -1,2 +1,3 @@
 // THIS IS the cart page
-// for adding here all the cart items
+// for adding here all the cart items.
+// adding with the cart items here will be data dependent stored in the local storage
