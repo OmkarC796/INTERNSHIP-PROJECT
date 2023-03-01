77f1738ba5bfa34ad9bb5b40899e2b7ccb9add0b
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-01 10:11:59 +0530

    new

---
+++ 01_Internship-project/demo/script.js
@@ -0,0 +1,7 @@
+function onclick() {
+    document.getElementById("menu").classList.toggle("icon");
+
+    document.getElementById("nav").classList.toggle("change");
+    
+    
+}
--- 01_Internship-project/script/cart.js
+++ 01_Internship-project/script/cart.js
@@ -1,3 +1,3 @@
-// THIS IS the cart page
+// This is the cart page.
 // for adding here all the cart items.
-// adding with the cart items here will be data dependent stored in the local storage
+// adding with the cart items here will be data dependent stored in the local storage.
